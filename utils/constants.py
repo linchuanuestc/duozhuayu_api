8883b842626e77e5558730e99a41a1bfc679e61f
linchuan <linchuan@360.cn>
2019-03-26 23:02:09 +0800

    add book error info

--- utils/constants.py
+++ utils/constants.py
@@ -5,13 +5,17 @@ ERR_UNKNOWN = 1001
 #user errno
 ERR_NO_USER = 2001
 
+#book errno
+ERR_NO_BOOK = 3001
+
 #db error
-ERR_DB      = 3001
+ERR_DB      = 4001
 
 #errmsg
 ERR_DESC = {
         ERR_SUC:     "",
         ERR_UNKNOWN: "unkown errors", 
         ERR_NO_USER: "not found the user", 
+        ERR_NO_BOOK: "not found the books", 
         ERR_DB:      "Db Error", 
         }
